3a031d83a07da83b6c867c52d158fe543f787729
farrukh90 <farrukhsadykov@gmail.com>
2022-03-25 10:43:31 +0000

    updated message

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("Node JS application V1 on" + os.hostname() + "\n");
+  response.end("Node JS application V2 on" + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
